55ab317714918ed4f228f200933be3ab3b06bc54
ankitsoniverma <ankitsoniverma614@gmail.com>
2026-07-05 13:06:56 +0530

    update

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,17 +1,38 @@
 import {Component} from 'react';
 import './index.css';
 class Login extends Component {
+
+    renderUsernameInputField = () => {
+        return(
+            <>
+                <label htmlfor="username" className="form-label">EMAIL </label>
+                <input type="text" id="username" placeholder="you@example.com" className="form-input"/>
+            </>
+        )
+    }
+    renderPasswordInputField = () => {
+        return(
+            <>
+                <label htmlfor="password" className="form-label">PASSWORD </label>
+                <input type="password" id="password" placeholder="Enter your password" className="form-input"/>
+            </>
+        )
+    }
+
     render(){
         return(
             <div className="login-main-container">
                 
                 <form className="login-form-container">
                     <h1 className="form-main-heading">Go Business</h1>
-                    <input type="text" />
-                    <input     type="checkbox" />
-                    <p>i win this july </p>
-                    <p>we all know is right</p>
-                    <p>vercel</p>
+                    <p className="form-sub-heading">Sign in to open your referral dashboard</p>
+                    <div className="input-container">
+                        {this.renderUsernameInputField()}
+                    </div>
+                    <div className="input-container">
+                        {this.renderPasswordInputField()}
+                    </div>
+                    <button type="submit" className="login-button">Sign In</button>
                 </form>
             </div>
         )
