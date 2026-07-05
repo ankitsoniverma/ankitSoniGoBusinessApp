ea1ee27dc5fd01f72206ec53e6eea77ad23edd9a
ankitsoniverma <ankitsoniverma614@gmail.com>
2026-07-05 10:44:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
+      <Login />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Login/index.js
@@ -0,0 +1,13 @@
+import {Component} from 'react';
+
+class Login extends Coompoent {
+    render(){
+        return(
+            <div>
+                <h1>Login</h1>
+            </div>
+        )
+    }
+}
+
+export default Login;
