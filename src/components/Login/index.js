e700f2c2394e09b8ffffc96bd58c5a133fdf1aa0
ankitsoniverma <ankitsoniverma614@gmail.com>
2026-07-05 12:43:56 +0530

    update

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -4,8 +4,9 @@ class Login extends Component {
     render(){
         return(
             <div className="login-main-container">
-                <h1>hello</h1>
-                <form>
+                
+                <form className="login-form-container">
+                    <h1 className="form-main-heading">Go Business</h1>
                     <input type="text" />
                     <input     type="checkbox" />
                     <p>i win this july </p>
